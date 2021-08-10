39b28b570d2b28b88cdfe94bfdd91a76858eb7c7
schonssj <jvschons@gmail.com>
2021-08-10 00:41:55 -0300

    Add project files.

---
+++ src/CleanCode.Names/AvoidWrongInfos/Examples.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CleanCode.Names.AvoidWrongInfos
+{
+    public class Examples
+    {
+        /// <summary>
+        /// A bad practice. Even if you would work in an hypotenuse context, this approach has a chance to create some mental confusion.
+        /// </summary>
+        public double HP;
+
+        /// <summary>
+        /// Another bad practice. The "List" word means something specific when talking about software dev. It has a chance to create some mental confusion.
+        /// </summary>
+        public Array AccountsList;
+
+        /// <summary>
+        /// Still a bad practice as it's redundant.
+        /// </summary>
+        public List<object> AnotherAccountsList;
+
+        /// <summary>
+        /// Now it's fine.
+        /// </summary>
+        public List<object> Accounts;
+        public List<object> AccountGroup;
+        public List<object> BunchOfAccounts;
+    }
+
+    /// <summary>
+    /// Preferentially avoid using identical names as this destroys fast reading.
+    /// </summary>
+    public class XYZControllerForEfficientHandlingOfStrings { }
+    public class XYZControllerForEfficientStorageOfStrings { }
+}
---
+++ src/CleanCode.Names/MeaningfulNames/Examples.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+namespace CleanCode.Names.MeaningfulNames
+{
+    public class Examples
+    {
+        #region Helper properties.
+        /// <summary>
+        /// Helper properties.
+        /// </summary>
+        public List<object> GameBoard;
+        public List<object> SomeList;
+        public int STATUS_VALUE;
+        public const int FLAGGED = 4;
+        #endregion Helper properties.
+
+        /// <summary>
+        /// A bad choice as this name doesn't have an expressive meaning.
+        /// </summary>
+        public int D;
+
+        /// <summary>
+        /// Good choices as these ones have a name which express their meaning.
+        /// </summary>
+        public int ElapsedTimeInDays;
+        public int DaysSinceCreation;
+        public int DaysSinceModification;
+        public int FileAgeInDays;
+
+        /// <summary>
+        /// Bad choice. It have an high implicitness.
+        /// For example, we don't know what comes within 'someList'.
+        /// Also, we don't know what is the meaning of 4, as well as the importance of the item at index 0.
+        /// </summary>
+        public List<int[]> GetThem()
+        {
+            List<int[]> list1 = new List<int[]>();
+
+            foreach (int[] i in SomeList)
+                if (i[0] == 4)
+                    list1.Add(i);
+            return list1;
+        }
+        
+        /// <summary>
+        /// Let's imagine that we are working with a minefield board. Each square is represented by a simple vector. And then, we realize that:
+        ///     - the 0 index stores a position;
+        ///     - the number 4 represents some 'flagged' rule;
+        ///     - and 'someList' is a cell list called GameBoard.
+        /// </summary>
+        public List<int[]> GetFlaggedCells()
+        {
+            List<int[]> flaggedCells = new List<int[]>();
+
+            foreach (int[] cell in GameBoard)
+                if (cell[STATUS_VALUE] == FLAGGED)
+                    flaggedCells.Add(cell);
+            return flaggedCells;
+        }
+
+        /// <summary>
+        /// Instead of continue using an int array, we can create a class for the cells, which can have a function that represents if the cell was flagged or not.
+        /// </summary>
+        public List<Cell> GetFlaggedCellsImproved()
+        {
+            List<Cell> flaggedCells = new List<Cell>();
+
+            foreach (Cell cell in GameBoard)
+                if (cell.IsFlagged)
+                    flaggedCells.Add(cell);
+            return flaggedCells;
+        }
+    }
+
+    public class Cell
+    {
+        public bool IsFlagged { get; }
+    }
+}
