1c99eec6fffea260a539f599aa1c74839c9e4131
schonssj <jvschons@gmail.com>
2021-08-11 19:38:41 -0300

    just added some samples about meaningful distinctions

---
+++ src/CleanCode.Names/MeaningfulDistinctions/Examples.cs
@@ -0,0 +1,61 @@
+﻿namespace CleanCode.Names.MeaningfulDistinctions
+{
+    public class Examples
+    {
+        /// <summary>
+        /// A bad practice as it's redundant.
+        /// </summary>
+        public string NameVariable;
+        public string NameString;
+        
+        /// <summary>
+        /// Some bad practices. We aren't able to understand their differences easily.
+        /// </summary>
+        public double MoneyAmount;
+        public double Money;
+
+        public object CustomerInfo;
+        public object Customer;
+        
+        public string TheMessage;
+        public string Message;
+        
+        public object AccountData;
+        public object Account;
+    }
+
+    /// <summary>
+    /// Some bad practices. Are you able to understand what is the difference between these classes only by their names?
+    /// </summary>
+    public class Product { }
+    public class ProductInfo { }
+    public class ProductData { }
+    
+    /// <summary>
+    /// Another bad practices. How do we'll know which one is better to have a payment history of a customer?
+    /// </summary>
+    public class Customer { }
+    public class CustomerObject { }
+
+    #region Helper classes.
+    public class Account { }
+    public class AccountInfo { }
+    #endregion
+
+    public class CustomerAccount
+    {
+        /// <summary>
+        /// Some bad practices. Which one do we should call to get data of an active account?
+        /// </summary>
+        public Account GetActiveAccount()
+        {
+            // Some database logic happens here...
+            return new Account();
+        }
+        public AccountInfo GetActiveAccountInfo()
+        {
+            // Some database logic happens here...
+            return new AccountInfo();
+        }
+    }
+}
