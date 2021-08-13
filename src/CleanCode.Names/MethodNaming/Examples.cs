fa8a3f1d45229646f52c8717924a115ac117a902
schonssj <jvschons@gmail.com>
2021-08-13 18:23:19 -0300

    just added some samples about methods naming

---
+++ src/CleanCode.Names/MethodNaming/Examples.cs
@@ -0,0 +1,31 @@
+﻿namespace CleanCode.Names.MethodNames
+{
+    public class Examples
+    {
+        /// <summary>
+        /// Access methods should be named with verbs which represents their actions.
+        /// </summary>
+        public int GetAge()
+        {
+            return 1;
+        }
+        
+        public void SetAge()
+        {
+            /// Some logic happens here...
+        }
+
+        /// <summary>
+        /// Boolean methods should be named like a question, where we can use a verb as a prefix followed by purpose of the verification.
+        /// </summary>
+        public bool HasLegalAge()
+        {
+            return false;
+        }
+        
+        public bool IsPhysicalPerson()
+        {
+            return true;
+        }
+    }
+}
