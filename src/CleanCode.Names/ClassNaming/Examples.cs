d8952b7c6c4675fb56647c8c7cf64f18b100f080
schonssj <jvschons@gmail.com>
2021-08-13 18:23:05 -0300

    just added some samples about class naming

---
+++ src/CleanCode.Names/ClassNaming/Examples.cs
@@ -0,0 +1,22 @@
+﻿namespace CleanCode.Names.ClassNaming
+{
+    public class Examples { }
+
+    /// <summary>
+    /// Classes should be named with substantives, such as the examples below.
+    /// </summary>
+    public class Customer { }
+    public class WikiPage { }
+    public class Account { }
+    public class AddressParser { }
+
+    /// <summary>
+    /// Classes also are not supposed to be named with verbs.
+    /// </summary>
+    public class WriteFile { }
+    
+    /// <summary>
+    /// We can improve the example above using a substantive and an adjective.
+    /// </summary>
+    public class FileWriter { }
+}
